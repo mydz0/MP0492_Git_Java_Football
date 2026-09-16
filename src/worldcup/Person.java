cb6a8c6defc018370e238d4465e457cfd453dbf8
TamaraBermudez <tamara.bermudez.vieras@gmail.com>
2026-09-16 16:41:27 +0200

    Clase person

--- src/worldcup/Person.java
+++ src/worldcup/Person.java
@@ -4,7 +4,15 @@ public class Person {
 
     int age;
     public void run(){
+        System.out.println("run");
+    }
 
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
     }
 
 }
