39cb5261f957e89ebd2820d68e690c0e15bfe0d4
mydz0 <stephanyarellano147@gmail.com>
2026-09-16 17:01:46 +0200

    Make style property private in Coach class

--- src/worldcup/Coach.java
+++ src/worldcup/Coach.java
@@ -4,7 +4,7 @@ package worldcup;
  * Class to define the coach
  */
 public class Coach extends Person {
-	string style;
+	private String style;
 
 	public void train() {
 		System.out.println("train");
